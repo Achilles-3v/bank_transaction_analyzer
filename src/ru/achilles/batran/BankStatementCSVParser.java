3fefd6f92dc5b0a0dd0c993bd4c723eee0a2678f
Achilles-3v <sh17vv@gmail.com>
2024-02-28 14:58:25 +0500

    added class BankTransaction

---
+++ src/ru/achilles/batran/BankStatementCSVParser.java
@@ -0,0 +1,11 @@
+package ru.achilles.batran;
+
+import java.time.format.DateTimeFormatter;
+
+public class BankStatementCSVParser {
+
+    private static final DateTimeFormatter DATE_PATTERN =
+            DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+
+}
