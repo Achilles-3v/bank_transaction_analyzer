d788430f725551a10280f7db92f6b32170d6bab9
Achilles-3v <sh17vv@gmail.com>
2024-02-29 18:41:33 +0500

    added BankStatementCSVParserTest

---
+++ src/test/java/BankStatementCSVParserTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import main.java.BankStatementCSVParser;
+import main.java.BankStatementParser;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+public class BankStatementCSVParserTest {
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+}
